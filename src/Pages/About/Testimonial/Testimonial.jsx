1323fdab2179c8bde07792e900db47553483d0aa
suvrodeb <suvrodev.cse.77@gmail.com>
2024-07-02 00:04:37 +0600

    Added testimonials json

--- src/Pages/About/Testimonial/Testimonial.jsx
+++ src/Pages/About/Testimonial/Testimonial.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TestimonialCard from "./TestimonialCard/TestimonialCard";
 
 const Testimonial = () => {
+  const [testimonials, setTestimonials] = useState([]);
+  useEffect(() => {
+    fetch("./testimonial.json")
+      .then((res) => res.json())
+      .then((data) => setTestimonials(data));
+  }, []);
+
+  console.log("Testimonials: ", testimonials);
   return (
     <div>
       <h1 className="text-2xl font-bold text-white mb-10">Testimonial</h1>
 
       <div>
         <div className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-bold text-white mb-10">
+          <h1 className="text-2xl font-bold text-white mb-10">
             OUR SATISFIED CUSTOMERS FEEDBACK
           </h1>
         </div>
