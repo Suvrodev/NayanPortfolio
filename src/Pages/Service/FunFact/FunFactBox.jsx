7e2d0da9a4517d32731c0d7daba058eceebb18dc
suvrodeb <suvrodev.cse.77@gmail.com>
2024-07-02 15:06:17 +0600

    modify fun fact

--- src/Pages/Service/FunFact/FunFactBox.jsx
+++ src/Pages/Service/FunFact/FunFactBox.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import CountUp from "react-countup";
 
 const FunFactBox = ({ fun }) => {
-  //   console.log(fun);
+  console.log(fun);
 
-  const { number, title, after } = fun;
+  const { number, title, after, botText } = fun;
   return (
     <div className="flex  items-center justify-center border-[1px] text-white gap-4 p-10">
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-8">
+        <div className="text-2xl font-bold text-center">{title}</div>
         <div className="flex items-center justify-center gap-1 text-2xl font-bold">
-          <h1>
-            <CountUp end={100} />
+          <h1 className="text-[60px] font-bold">
+            <CountUp end={number} />
           </h1>
-          <p>{after}</p>
         </div>
-        <div className="text-[18px]">{title}</div>
+        <div className="text-2xl font-bold text-center">{botText}</div>
       </div>
     </div>
   );
