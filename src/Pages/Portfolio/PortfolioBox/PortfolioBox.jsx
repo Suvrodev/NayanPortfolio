341fbc1f564b041356b28458e5f09813a0cc33d5
suvrodeb <suvrodev.cse.77@gmail.com>
2024-07-04 00:02:14 +0600

    Added Modal

--- src/Pages/Portfolio/PortfolioBox/PortfolioBox.jsx
+++ src/Pages/Portfolio/PortfolioBox/PortfolioBox.jsx
@@ -5,7 +5,23 @@ const PortfolioBox = ({ portfolio }) => {
   const { image, category } = portfolio;
   return (
     <div>
-      <img src={image} alt="" className="w-[400px]" />
+      <dialog id="my_modal_3" className="modal ">
+        <div className="modal-box p-0">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-circle btn-ghost absolute right-2 top-2 bg-red-600 text-white z-10">
+              ✕
+            </button>
+          </form>
+          <div className="p-0">
+            <img src={image} alt="" />
+          </div>
+        </div>
+      </dialog>
+
+      <div onClick={() => document.getElementById("my_modal_3").showModal()}>
+        <img src={image} alt="" className="w-[400px]" />
+      </div>
     </div>
   );
 };
