81c961f310914f58332e7f09092b7f16d68454f0
suvrodeb <suvrodev.cse.77@gmail.com>
2024-07-06 22:37:44 +0600

    Added mobile header

--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../../Shared/Header/Header/Header";
 import "./Main.css";
 import Cursor from "./Cursor/Cursor";
+import MobileHeader from "../../Shared/Header/Header/MobileHeader/MobileHeader";
 
 const Main = () => {
   return (
@@ -14,6 +15,9 @@ const Main = () => {
         <div className="w-full md:w-[75%] bg-[#333333]">
           <div className="hidden md:block">
             <Cursor />
+          </div>
+          <div className="md:hidden">
+            <MobileHeader />
           </div>
           <Outlet />
         </div>
