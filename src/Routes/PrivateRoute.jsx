7715c62db091bdb683ecbcb46a7af579c772313d
suvrodeb <suvrodev.cse.77@gmail.com>
2024-07-14 08:10:49 +0600

    Added mail privacy in private route

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
 
@@ -6,12 +6,23 @@ const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
   // console.log(location)
+
+  //   const [confirmMail, setConfirmMail] = useState(false);
+  //   useEffect(() => {
+  //     const mail = user?.email;
+  //     console.log("Mail: ", mail);
+  //     if (mail == "suvrodevhowlader@gmail.com") {
+  //       setConfirmMail(true);
+  //     }
+  //   }, [user, confirmMail]);
+  //   console.log("Confirm Mail: ", confirmMail);
+
   if (loading) {
     return (
       <progress className="progress w-56 bg-green-400 text-green-400"></progress>
     );
   }
-  if (user) {
+  if (user && user?.email == "suvrodevhowlader1408@gmail.com") {
     return children;
   }
 
