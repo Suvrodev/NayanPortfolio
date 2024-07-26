cc5220b55a99be911ee6898fde76af0f838da4a8
suvrodeb <suvrodev.cse.77@gmail.com>
2024-07-27 04:21:01 +0600

    Added funfact animation

--- src/Pages/Service/FunFact/FunFactBox.jsx
+++ src/Pages/Service/FunFact/FunFactBox.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
+import { useInView } from "react-intersection-observer";
 
 const FunFactBox = ({ fun }) => {
   // console.log(fun);
 
   const { number, title, after, botText } = fun;
+
+  const [counterOn, setCounterOn] = useState(false);
+  const handleShowCounter = () => {
+    setCounterOn(!counterOn);
+  };
+
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0,
+  });
+
+  // console.log("InView: ", inView);
 
   return (
     <div className="flex  items-center justify-center border-[1px] c gap-4 p-10">
@@ -12,7 +26,12 @@ const FunFactBox = ({ fun }) => {
         <div className="text-2xl font-bold text-center">{title}</div>
         <div className="flex items-center justify-center gap-1 text-2xl font-bold">
           <h1 className="text-[60px] font-bold">
-            <CountUp end={number} />
+            <ScrollTrigger
+              onEnter={handleShowCounter}
+              onExit={handleShowCounter}
+            >
+              <div ref={ref}>{counterOn && <CountUp end={number} />}</div>
+            </ScrollTrigger>
           </h1>
         </div>
         <div className="text-2xl font-bold text-center">{botText}</div>
@@ -22,3 +41,11 @@ const FunFactBox = ({ fun }) => {
 };
 
 export default FunFactBox;
+
+/**
+ * urls: https://www.npmjs.com/package/react-intersection-observer
+ */
+
+/**
+ * urls: //www.youtube.com/watch?v=NWyWoW61B3I&t=2s
+ */
